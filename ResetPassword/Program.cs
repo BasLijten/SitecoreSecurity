f8be89e67d5852f5d5ae3a2fcf73b12fb048684a
Bas Lijten <bas.lijten@gmail.com>
2016-08-29 15:36:26 +0200

    Added Reset password tool for Sitecore

---
+++ ResetPassword/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Security;
+
+namespace ResetPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Security.Cryptography.CryptoConfig.AddAlgorithm(typeof(Zetetic.Security.Pbkdf2Hash), "pbkdf2_local");
+
+            string userId = @"sitecore\admin";
+            string passwd = String.Empty;
+            MembershipUser user = Membership.GetUser(userId, false);
+            user.ResetPassword();
+
+            Console.WriteLine(passwd);
+            Console.ReadLine();
+        }
+    }
+}
